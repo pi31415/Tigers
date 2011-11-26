211cdbf7c81b8dd90a6fb4583b0f02ebed039a1a
Feynman RP <feynmanwobble@gmail.com>
2011-11-26 14:03:18 -0800

    Removed several errors

--- sample/src/main/java/com/apitech/App.java
+++ sample/src/main/java/com/apitech/App.java
@@ -9,9 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-
-        // TODO - Add stuff in if statement
-	if (true) {
-	}
     }
 }
