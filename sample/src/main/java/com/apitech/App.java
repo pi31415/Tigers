cd637d1fd48b947f6644f6fa7316e5c1724b3d8d
Feynman RP <feynmanwobble@gmail.com>
2011-11-26 14:16:16 -0800

    Format Auto

--- sample/src/main/java/com/apitech/App.java
+++ sample/src/main/java/com/apitech/App.java
@@ -5,6 +5,10 @@ package com.apitech;
  */
 public class App {
 
+    /**
+     * Entry for running application
+     * @param args Not used
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
