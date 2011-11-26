05bd387292d0fa3fe24013692c5813d6295adac8
Feynman RP <feynmanwobble@gmail.com>
2011-11-26 14:20:48 -0800

    Format Auto

--- sample/src/main/java/com/apitech/App.java
+++ sample/src/main/java/com/apitech/App.java
@@ -6,10 +6,27 @@ package com.apitech;
 public class App {
 
     /**
+     * Test String
+     */
+    private String testString;
+
+    /**
      * Entry for running application
      * @param args Not used
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    /**
+     * Standard getter
+     * @return
+     */
+    public String getTestString() {
+        return testString;
+    }
+
+    public void setTestString(String testString) {
+        this.testString = testString;
     }
 }
